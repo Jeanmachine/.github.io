71cda6522f660470dc5a6ac8986667df2cabadc5
Jeanmachine <35971368+Jeanmachine@users.noreply.github.com>
2018-04-10 16:12:43 -0700

    added blank example

---
+++ p5-blankExample/sketch.js
@@ -0,0 +1,18 @@
+
+// declare variables here
+
+
+// setup runs once
+function setup() {
+	createCanvas(2000,2000);
+	background(150);
+}
+
+// draw loops and loops
+function draw() {
+ellipse(121,192,123);
+
+}
+
+
+// write functions here
